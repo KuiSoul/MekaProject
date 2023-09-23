2ee034dfe9e394e8084f66ddef58a412afeb8052
KuiSoul <soulscholar0912@gmail.com>
2023-09-23 00:14:17 -0700

    Test

--- FlaskBlogApp/routes.py
+++ FlaskBlogApp/routes.py
@@ -129,8 +129,8 @@ def offers():
     if request.method == "POST":
         # Perform filtering
         filters = request.form.get("filters")  # Replace "filter_value" with the name of your filter input
-        if (filters == None):
-            return render_template("base.html", offers=the_base)
+        # if (filters == None):
+        #     filters = ''
         query = Offer.query.filter(Offer.offer_body.contains(filters) | Offer.offer_title.contains(filters))  # Replace "filter_column" with the applicable column name in your Offer model
     else:
         # Display all data
